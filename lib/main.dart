cb8bc6d49950cf0f8938481774dc8d164ef4b8e2
shawnsun123 <shawn.sun@dragonagile.com>
2021-09-18 14:03:37 -0400

    First addition

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+
+import './MainPage.dart';
+
+void main() => runApp(new ExampleApplication());
+
+class ExampleApplication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MainPage());
+  }
+}
